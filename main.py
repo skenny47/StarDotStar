e9d28779ce85fbf27ccec3a889249b9811f97953
EC2 Default User <ec2-user@ip-172-31-37-217.us-west-2.compute.internal>
2018-08-17 18:06:00 +0000

    CORS by hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, send_from_directory, json
-from flask_cors import CORS, cross_origin
+from flask import Flask, render_template, send_from_directory, json, jsonify
+#from flask_cors import CORS, cross_origin
 
 server_globals = {
     "parts" : "4",
@@ -8,16 +8,18 @@ server_globals = {
 
 app = Flask(__name__,static_url_path = "", static_folder = ".")
 app.config.from_pyfile('settings/development_settings.cfg')
-CORS(app)
+#CORS(app)
 
 @app.route("/performer", methods=['GET'])
 def index():
     return send_from_directory('.','performer.html')
     
 @app.route("/globals", methods=['GET'])
-@cross_origin(origin='*')
+#@cross_origin(origin='*')
 def globals():
-    return json.dumps(server_globals)
+    response = jsonify(json.dumps(server_globals))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", debug = True, port = 4747)
