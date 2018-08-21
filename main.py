092734291624db1206c0b02bd743cc60330071d9
EC2 Default User <ec2-user@ip-172-31-37-217.us-west-2.compute.internal>
2018-08-21 17:08:03 +0000

    adding endpoint to main.py for events storage

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, send_from_directory, json, jsonify
-#from flask_cors import CORS, cross_origin
+import datetime
 import globals
 
 server_globals = globals.server_globals
+
+class StarDotStarEvent():
+    def __init__(self,x,y,icon,time):
+        self.x = x
+        self.y = y
+        self.icon = icon
+        self.time = time
+    
+
+events = [ ]
 
 app = Flask(__name__,static_url_path = "", static_folder = ".")
 app.config.from_pyfile('settings/development_settings.cfg')
@@ -19,5 +29,13 @@ def globals():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+@app.route('/newEvent')
+def newEvent():
+  x = request.args.get('x', default = 0, type = int)
+  y = request.args.get('y', default = 0, type = int)
+  icon = request.args.get('icon', default = '*', type = str)
+  events.append(StarDotStarEvent(x,y,icon,datetime.datetime.now()))
+  return
+  
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", debug = True, port = 4747)
