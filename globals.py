3eb281424aa9aabf58c37f8b479148d855e4d30d
EC2 Default User <ec2-user@ip-172-31-37-217.us-west-2.compute.internal>
2018-09-03 16:17:52 +0000

    tryin gagain for table driven icons

--- globals.py
+++ globals.py
@@ -1,5 +1,10 @@
-
 server_globals = {
     "parts" : "4",
-    "other" : "undefined"
+    "icons" : 
+        [ 
+            {"fileName" : "icons8-music-50.png", "name" : "eighthNotes"},
+            {"fileName" : "icons8-pause-button-50.png", "name" : "pause"},
+            {"fileName" : "icons8-mcdonald`s-french-fries-50.png", "name" : "fries"},
+            {"fileName" : "icons8-french-music-50.png", "name" : "accordian"},
+        ]
 }
