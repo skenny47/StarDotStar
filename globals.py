22a1c23291c9ea90fda0ebb1f85e9915e083430f
EC2 Default User <ec2-user@ip-172-31-37-217.us-west-2.compute.internal>
2018-09-06 03:28:20 +0000

    new icons. more icons

--- globals.py
+++ globals.py
@@ -3,11 +3,15 @@ server_globals = {
     "lambda" : "1000",
     "icons" : 
         [ 
-            {"fileName" : "icons8-music-50.png", "name" : "eighthNotes"},
-            {"fileName" : "icons8-pause-button-50.png", "name" : "pause"},
+            {"fileName" : "icons8-banana-50.png", "name" : "banana"},
+            {"fileName" : "icons8-chili-pepper-50.png", "name" : "pepper"},
+            {"fileName" : "icons8-confusion-50.png", "name" : "confusion"},
+            {"fileName" : "icons8-corn-50.png", "name" : "corn"},
+            {"fileName" : "icons8-hamburger-50.png", "name" : "hamburger"},
             {"fileName" : "icons8-mcdonald`s-french-fries-50.png", "name" : "fries"},
-            {"fileName" : "icons8-french-music-50.png", "name" : "accordian"},
-            {"fileName" : "icons8-microphone-50.png", "name" : "microphone"},
-            {"fileName" : "icons8-uc-browser-50.png", "name" : "jessica"},
+            {"fileName" : "icons8-music-50.png", "name" : "notes"},
+            {"fileName" : "icons8-octopus-50.png", "name" : "octopus"},
+            {"fileName" : "icons8-spam-can-50.png", "name" : "spam"},
+            {"fileName" : "minnesota_vikings_2013-pres.png", "name" : "vikings"},
         ]
 }
